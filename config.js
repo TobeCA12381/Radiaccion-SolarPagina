d2225f38131806b372aa7d62ce0060e341d5d342
TobeCA12381 <TobeCA12381>
2024-07-28 19:32:37 -0500

    xd

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-// config.js
 module.exports = {
     PORT: process.env.PORT || 8093,
-    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_HOST: process.env.DB_HOST || 'monorail.proxy.rlwy.net',
     DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || '',
-    DB_NAME: process.env.DB_NAME || 'grupo3',
-    DB_PORT: process.env.DB_PORT || 3306
+    DB_PASSWORD: process.env.DB_PASSWORD || 'HqdHeeXAqjNUdHRLRBrpAbNSHWcBjkxn',
+    DB_NAME: process.env.DB_NAME || 'railway',
+    DB_PORT: process.env.DB_PORT || 53829
 };
