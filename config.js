7e3be6ab8d5307a92e5d0f8452999ed7e9d9357a
Carlos-Alarcon <168382947+TobeCA12381@users.noreply.github.com>
2024-07-30 13:17:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8093,
-    DB_HOST: process.env.DB_HOST || 'monorail.proxy.rlwy.net',
+    DB_HOST: process.env.DB_HOST || 'mysql.railway.internal',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || 'HqdHeeXAqjNUdHRLRBrpAbNSHWcBjkxn',
     DB_NAME: process.env.DB_NAME || 'railway',
-    DB_PORT: process.env.DB_PORT || 53829
+    DB_PORT: process.env.DB_PORT || 3306
 };
